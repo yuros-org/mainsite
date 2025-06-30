743fcb63b5a5f72cc05255d92e109af50c33e080
almuhdilkarim <almuhdilkarim2@mail.com>
2025-06-30 16:35:07 +0700

    Mon Jun 30 16:35:06 WIB 2025

--- assets/scripts/module/course/home/pack/script.js
+++ assets/scripts/module/course/home/pack/script.js
@@ -1,1 +1,1 @@
-(()=>{console.log("Script : critical fallback replacement"),console.log("deffers home course javascript engine loaded");})();
+(()=>{console.log("Script : critical fallback replacement"),console.log("deffers header core model script loaded"),console.log("deffers footer core themes script loaded"),console.log("deffers home course javascript engine loaded");})();

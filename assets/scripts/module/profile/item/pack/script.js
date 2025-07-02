b6f72812378826a32384612781541a8c1f78fe16
almuhdilkarim <almuhdilkarim2@mail.com>
2025-07-02 19:29:56 +0700

    Wed Jul  2 19:29:56 WIB 2025

--- assets/scripts/module/profile/item/pack/script.js
+++
@@ -1,1 +0,0 @@
-(()=>{console.log("Script : critical fallback replacement"),console.log("deffers header core model script loaded"),console.log("deffers footer core themes script loaded");})();
--- assets/scripts/page/home/pack/script.js
+++ assets/scripts/page/home/pack/script.js
@@ -1,1 +1,1 @@
-(()=>{console.log("Script : critical fallback replacement"),console.log("deffers header core model script loaded"),console.log("deffers footer core themes script loaded");})();
+(()=>{console.log("deffer moscow theme default engine loaded"),console.log("Script : critical fallback replacement");})();
